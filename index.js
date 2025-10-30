0946e90a2f01bcaf7b3dbe72358df7c4c9a1011b
arufiand <alfiandannyarmanta@gmail.com>
2025-10-31 01:39:38 +0700

    feat: implement basic posts service with Express and body-parser

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const randomBytes = require('crypto').randomBytes;
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+const posts = {}
+
+// Sample route
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {
+        id,
+        title
+    };
+
+
+    res.status(201).send(posts[id]);
+});
+
+app.listen(port, () => {
+    console.log(`Posts service is running at http://localhost:${port}`);
+});
